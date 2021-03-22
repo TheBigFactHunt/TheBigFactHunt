fa5c6fb2ddab30dcd2ab26a7e25a5e404d1be5e4
Nicholas Ferrari <Nick.ferrari@me.com>
2021-03-22 15:41:39 +0000

    changing link css

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <div className="footerWrapper">
       <div className="gitLinks">
-        <a href="https://github.com/NickFerra">Nick</a>
-        <a href="https://github.com/JoeFoster-cn">Joe</a>
-        <a href="https://github.com/jthomasw88">Jordan</a>
-        <a href="https://github.com/SteIddon">Ste</a>
+        <a className="footerLinks" href="https://github.com/NickFerra">Nick</a>
+        <a className="footerLinks" href="https://github.com/JoeFoster-cn">Joe</a>
+        <a className="footerLinks" href="https://github.com/jthomasw88">Jordan</a>
+        <a className="footerLinks" href="https://github.com/SteIddon">Ste</a>
       </div>
         <div className="footerWho">
-            <p>This has been created by <a href="https://github.com/TheBigFactHunt">TheBigFactHunt</a> organisation</p>
+            <p>This has been created by <a className="footerLinks" href="https://github.com/TheBigFactHunt">TheBigFactHunt</a> organisation</p>
         </div>
     </div>
   );
