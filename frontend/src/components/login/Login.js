d9efde60460bd6f78153371a149c35d158e99792
Joe Foster <jfoster29492@gmail.com>
2021-03-22 14:15:35 +0000

    added components for navbar, login, signup
    for testing sign up / database

---
+++ frontend/src/components/login/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./login.css";
+
+const Login = () => {
+    return (
+        <div className="loginBox">
+            <h3 id="emailBar">Email&nbsp;<input className="eBar" type="text" placeholder="Email..."></input></h3>
+            <h3 id="passBar">Password&nbsp;<input className="pBar" type="password" placeholder="Password"></input></h3>
+            <a className="sBtn">Sign In</a>&nbsp;
+            <a className="signUpBtn" href="/signup">Sign Up</a>&nbsp;
+        </div>
+    )
+}
+
+export default Login;
