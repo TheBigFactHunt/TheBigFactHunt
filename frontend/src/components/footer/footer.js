438ded63870f2104ce9a45cb0a49391c15a2614c
Nicholas Ferrari <Nick.ferrari@me.com>
2021-03-22 15:48:26 +0000

    formatting

--- frontend/src/components/footer/footer.js
+++ frontend/src/components/footer/footer.js
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className="footerWrapper">
       <div className="gitLinks">
-        <a className="footerLinks" href="https://github.com/NickFerra">Nick</a>
-        <a className="footerLinks" href="https://github.com/JoeFoster-cn">Joe</a>
-        <a className="footerLinks" href="https://github.com/jthomasw88">Jordan</a>
-        <a className="footerLinks" href="https://github.com/SteIddon">Ste</a>
+        <a className="footerLinks" href="https://github.com/NickFerra">Nick </a>
+        <a className="footerLinks" href="https://github.com/JoeFoster-cn">Joe </a>
+        <a className="footerLinks" href="https://github.com/jthomasw88">Jordan </a>
+        <a className="footerLinks" href="https://github.com/SteIddon">Ste </a>
       </div>
         <div className="footerWho">
             <p>This has been created by <a className="footerLinks" href="https://github.com/TheBigFactHunt">TheBigFactHunt</a> organisation</p>
