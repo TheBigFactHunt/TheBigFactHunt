02f0643f4a26fd6c1d9650158cc3d03e566c1a8e
Nicholas Ferrari <Nick.ferrari@me.com>
2021-03-23 15:02:00 +0000

    adding high score column

--- frontend/src/router/AppRouter.js
+++ frontend/src/router/AppRouter.js
@@ -6,8 +6,9 @@ import PartTwo from "../components/PartTwo";
 import PartThree from "../components/PartThree";
 import Login from "../components/Login";
 import Footer from "../components/footer/footer";
-import Sidebar from "../components/sidebar/Sidebar";
-import Social_feed from "../components/social_feed/Social_feed";
+import Sidebar from "../components/sidebar/sidebar";
+import Social_feed from "../components/social_feed/social_feed";
+import HighScore from "../components/highScore/highScore";
 import Home from "../components/Home";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
@@ -32,8 +33,9 @@ const AppRouter = () => {
                     <Route component={Login} path="/login"/>
                 </Switch>
                 {/* to be moved */}
-                <div>
+                <div className="indivdual_components">
                     <Social_feed />
+                    <HighScore />
                 </div>
                 <div className="footerContainer">
                     <Footer />
