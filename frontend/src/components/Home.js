574d4de1f3c874d282e75e8f39ed42ca9c332391
Nicholas Ferrari <Nick.ferrari@me.com>
2021-03-24 16:01:20 +0000

    conflict correction

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,25 +5,22 @@ import HighScore from "../components/highScore/highScore";
 import "./home.css";
 
 const Home = (props) => {
-  console.log(props);
-  return (
-    <div className="homeContainer">
-      <div className="loggedIn">
-        <h3 id="loggedInText">Logged in as</h3>{" "}
-        <p id="loggedInInfo">
-          {props.user.first_name} {props.user.last_name}{" "}
-        </p>
-      </div>
-      {/* <h1 id="welcomeText">The Big Fact Hunt</h1> */}
-      <div className="logoContainer">
-        <img id="logoImg" src={Logo} alt="Logo image" />
-      </div>
-      {/* to be moved */}
-      <div className="individual_components">
-        <Social_feed />
-        <HighScore />
-      </div>
-    </div>
-  );
-};
+    console.log(props)
+    return(
+        <div className="homeContainer">
+                <div className="loggedIn">
+                    <h3 id="loggedInText">Logged in as</h3> <p id="loggedInInfo">{props.user.first_name} {props.user.last_name} </p>
+                </div>
+            {/* <h1 id="welcomeText">The Big Fact Hunt</h1> */}
+                <div className="logoContainer">
+                    <img id="logoImg" src={Logo} alt="Logo image" />
+                </div>
+                {/* to be moved */}
+          <div className="indivdual_components">
+            <Social_feed />
+            <HighScore user={props.user}/>
+          </div>
+        </div>
+    )
+}
 export default Home;
