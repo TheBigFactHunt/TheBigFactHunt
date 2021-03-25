2770ff8e4cdffaab64ec4d959852c57eb71d2d5e
Nicholas Ferrari <Nick.ferrari@me.com>
2021-03-25 14:06:00 +0000

    fixing question and score counter

--- frontend/src/components/quiz/Question.js
+++ frontend/src/components/quiz/Question.js
@@ -1,36 +1,27 @@
 import React, { useState, useEffect } from "react";
 
 const Question = (props) => {
-  //   const answerTrue = (props) => {
-  //     props.question.correct_answer ? props.setCounter(props.counter +1) : props.setCounter(props.counter +2)
-  //     // if (props.question.correct_answer == "True") {
-  //     //   props.setCounter(props.counter + 1);
-  //     // }
-  //     // elseif()
-  //   };
   return (
     <div class Name="quizQuestions">
       <p>{props.question.question}</p>
       <button
         id="answerButton"
         onClick={() => {
-          // if(props.question.correct_answer == "True"){
-          //     props.setCounter(props.counter+1)
-          // }
-        props.question.correct_answer
-            ? props.setCounter(props.counter + 1)
-        : props.question.incorrect_answer 
-            ? props.setCounter(props.counter + 2)
+          if(props.question.correct_answer == "True"){
+              props.setScore(props.score + 1)
+          }
+          props.setCounter(props.counter+1)
+
         }}
-      >
-        True
+      >True
       </button>
       <button
         id="answerButton"
         onClick={() => {
-          if(props.question.incorrect_answer == "True"){
-              props.setCounter(props.counter+1)
+          if(props.question.correct_answer == "False"){
+              (props.setScore(props.score + 1))
           }
+          props.setCounter(props.counter+1)
         }}
       >
         False
