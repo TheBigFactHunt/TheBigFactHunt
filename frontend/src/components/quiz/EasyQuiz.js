09103426b64372c1d5a7cdae718e9586ddef7f7e
Ste Iddon <steiddon@gmail.com>
2021-03-26 15:45:24 +0000

    alerts with correct/incorrect messages

--- frontend/src/components/quiz/EasyQuiz.js
+++ frontend/src/components/quiz/EasyQuiz.js
@@ -3,12 +3,14 @@ import Question from "./Question";
 import "./quiz.css";
 import Logo from "../../images/logos/logo-small-yellow+purple.png";
 import SubmitScore from './submitScore'
+import SocialPost from '../social_feed/social_posts'
 
 
 const EasyQuiz = (props) => {
   const [questions, setQuestions] = useState([]);
   const [counter, setCounter] = useState(0);
   const [score, setScore] = useState(0);
+  const difficulty = "easy";
   useEffect(() => {
     const getter = async () => {
       const response = await fetch(
@@ -30,7 +32,7 @@ const EasyQuiz = (props) => {
         />
       </h3>
 
-      <h2 id="scoreCounter">Score: {score}</h2>
+      <h2 id="scoreCounter">Score: {score} / {counter} </h2>
       {questions.length > 0 && counter < 10 ? (
         <Question
           question={questions[counter]}
@@ -42,6 +44,8 @@ const EasyQuiz = (props) => {
       ) : null}
       <br></br>
       {counter >=10 ? <SubmitScore score={score} /> : null }
+      <br></br>
+      {counter >=10 ? <SocialPost score={score} name={props.first_name} /> : null}
     </div>
   );
 };
--- frontend/src/components/quiz/Question.js
+++ frontend/src/components/quiz/Question.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Swal from 'sweetalert2';
 
 const Question = (props) => {
   return (
@@ -9,9 +10,12 @@ const Question = (props) => {
         onClick={() => {
           if(props.question.correct_answer == "True"){
               props.setScore(props.score + 1)
-          }
+              Swal.fire("Correct!")
+              props.setCounter(props.counter+1)
+          } else {
           props.setCounter(props.counter+1)
-
+          Swal.fire("Sorry, wrong answer")
+          }
         }}
       >True
       </button>
@@ -19,9 +23,12 @@ const Question = (props) => {
         id="answerButton"
         onClick={() => {
           if(props.question.correct_answer == "False"){
-              (props.setScore(props.score + 1))
-          }
+              props.setScore(props.score + 1)
+              Swal.fire("Correct!")
+              props.setCounter(props.counter+1)
+          } else {
           props.setCounter(props.counter+1)
+          }
         }}
       >
         False
