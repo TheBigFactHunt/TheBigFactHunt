1db984638a973386825c3078d584e0e6cf35adad
jordan <jthomasw88>
2021-04-01 11:04:21 +0100

    about animation

--- frontend/src/components/about/About.js
+++ frontend/src/components/about/About.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React from "react";
 import "./about.css";
 
 const About = () => {
-    return (
-        <div className="about">
-            <h1 id="aboutTitle">About The Big Fact Hunt</h1>
-            <p id="aboutText">The Big Fact Hunt was created by 
-            <a id="ghLink" href="https://github.com/NickFerra"> Nick, </a> 
-            <a id="ghLink" href="https://github.com/JoeFoster-cn">Joe, </a> 
-            <a id="ghLink" href="https://github.com/jthomasw88"> Jordan </a> and  
-            <a id="ghLink" href="https://github.com/SteIddon"> Ste </a> 
-            as the final project of our <a id="codenation" href="https://wearecodenation.com/">Code Nation </a>
-            12-week bootcamp.
-            </p>
-        </div>
-    )
-}
+  return (
+    <div className="about">
+      <div className="animate__animated animate__flipInX" id="divani">
+        <h1 id="aboutTitle">About The Big Fact Hunt</h1>
+        <p id="aboutText">
+          The Big Fact Hunt was created by
+          <a id="ghLink" href="https://github.com/NickFerra">
+            {" "}
+            Nick,{" "}
+          </a>
+          <a id="ghLink" href="https://github.com/JoeFoster-cn">
+            Joe,{" "}
+          </a>
+          <a id="ghLink" href="https://github.com/jthomasw88">
+            {" "}
+            Jordan{" "}
+          </a>{" "}
+          and
+          <a id="ghLink" href="https://github.com/SteIddon">
+            {" "}
+            Ste{" "}
+          </a>
+          as the final project of our{" "}
+          <a id="codenation" href="https://wearecodenation.com/">
+            Code Nation{" "}
+          </a>
+          12-week bootcamp.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default About;
